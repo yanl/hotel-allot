80d1c21c7a9ee1db13b2f6a0e8ef285c9cb81892
Yan Leroy <yan.leroy@gmail.com>
2012-11-27 13:25:11 +0400

    Dynamic Description in Room's Select2 placeholder

--- js/app.js
+++ js/app.js
@@ -19,7 +19,11 @@ angular.module('components', []).
 			}
 			$http.get('/DMS/components/hotel_allot.cfc?method=getRooms&idHotel='+$scope.idHotel, {cache:true}).success(function(data) {
 			   $scope.rooms = data;
-			   // todo: update place-holder with # of rooms or "no rooms found"
+			   var placeholder = 'No rooms found';
+			   if ($scope.rooms.length) {
+				  placeholder = $scope.rooms.length + ' rooms';
+			   }
+			   $($element).find('.view-room').attr('placeholder', placeholder);
 			});
 		 };
 		 
